b26e4d037feec632cd241e4a25e2be963b707bd4
maav1 <maav1@yandex.ru>
2021-03-20 19:46:52 +0300

    Готово.

--- Module14/07_years/main.py
+++ Module14/07_years/main.py
@@ -1,1 +1,6 @@
-# TODO здесь писать код
+
+one_year = int(input('Введите первый год: '))
+two_year = int(input('Введите второй год: '))
+for i in range (one_year,two_year):
+    if (i % 10 == (i // 10) % 10 and i % 10 == (i // 100) %10) or (i % 10 ==(i // 10) % 10 and i % 10 == i // 1000) or (i % 10 == (i // 100) % 10 and i % 10 == i // 1000) or ((i // 10) % 10 == (i // 100) % 10 and ((i // 10) % 10 == i // 1000)):
+        print(i)

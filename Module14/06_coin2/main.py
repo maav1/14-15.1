bdf5996191c26512c182f29f120e27777041422a
maav1 <maav1@yandex.ru>
2021-03-20 15:21:41 +0300

    Готово.

--- Module14/06_coin2/main.py
+++ Module14/06_coin2/main.py
@@ -1,1 +1,10 @@
-# TODO здесь писать код
+def calculating(x,y,r):
+    if x ** 2 + y ** 2 <= r ** 2:
+        print('Монетка рядом.')
+    else:
+        print('Монетка далеко.')
+print('Введите координаты монетки:')
+x = float(input('X: '))
+y = float(input('Y: '))
+r = int(input('Введите радиус: '))
+calculating(x,y,r)

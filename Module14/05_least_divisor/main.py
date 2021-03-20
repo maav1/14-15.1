36bfcc25504bddae65f1a60b225aed4fded1ff88
maav1 <maav1@yandex.ru>
2021-03-20 13:10:50 +0300

    Готово.

--- Module14/05_least_divisor/main.py
+++ Module14/05_least_divisor/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+def nok(number):
+    for i in range(2,number+1):
+        if number % i > 0:
+            continue
+        else:
+            break
+    return i
+number = int(input('Введите число для которого надо найди НОК: '))
+print('Наименьший делитель, отличный от единицы:',nok(number))

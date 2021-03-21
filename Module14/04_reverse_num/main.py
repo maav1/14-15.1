245ad0868249d4d920184e4d9b33105541187ee8
maav1 <maav1@yandex.ru>
2021-03-21 22:57:25 +0300

    Готово. Описание в README.md

--- Module14/04_reverse_num/main.py
+++ Module14/04_reverse_num/main.py
@@ -1,3 +1,48 @@
-# TODO здесь писать код
 
 
+def enter_data(point):
+    number1 = input('Введите первое число: ')
+    number2 = input('Введите второе число: ')
+    return number1 + '. ' + number2 + '. '
+
+
+def number_revers(number):
+    num_revers = number[::-1]
+    return num_revers
+
+
+point = ['.']
+number1 = enter_data(point)
+probel = [' ']
+total_num =number = []
+second_point = 0
+n = ''
+number_one_revers = []
+for symb in number1:
+    if symb != '.' and symb != ' ':
+        number += symb
+    elif symb == '.':
+        second_point += 1
+        n = number_revers(number)
+        if second_point != 2:
+            number_one_revers += (n + point)
+        else:
+            number_one_revers += (n + probel)
+            second_point = 0
+        number = []
+    else:
+        total_num = ("".join(number_one_revers))
+flag = 0
+num1 = num2 = ''
+for i in total_num:
+    if i == ' ':
+        flag = 1
+
+    if not flag:
+        num1 += i
+    else:
+        num2 += i
+b1 = float(num1)
+c1 = float(num2)
+summ = round(b1 + c1,3)
+print('\nПервое число наоборот:',b1,'\nВторое число наоборот:',c1,'\n\nИх сумма:',summ)

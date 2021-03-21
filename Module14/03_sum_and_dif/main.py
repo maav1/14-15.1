8aee42c8dd0b5c83af581121ce9506381b107848
maav1 <maav1@yandex.ru>
2021-03-21 22:54:42 +0300

    Готово.

--- Module14/03_sum_and_dif/main.py
+++ Module14/03_sum_and_dif/main.py
@@ -2,19 +2,22 @@
 
 def finding_sum_digits(number):
     summ_digit = 0
-    for digit in range(number):
+    for digit in number:
         summ_digit += int(digit)
-    return summ_digit
+    return summ_digit #Возвращает сумму
 
-def quantity_digit(number):
+
+def quantity_digit(number,summ_digit):
     count = 0
-    while number > 1:
+    while number != 0:
         count += 1
         number = number // 10
-    diff = finding_sum_digits(number) - count
-    return count,diff
+    diff = summ_digit - count
+    return count,diff #Возвращает количество и разность
 
 number = input('Введите число: ')
 print('Сумма цифр:',finding_sum_digits(number))
-print('Колличество цифр:',quantity_digit(int(number)))
-print('Разность суммы и кол-ва цифр:',quantity_digit(int(number)))
+summ_digit = finding_sum_digits(number)
+count,diff = quantity_digit(int(number),summ_digit)
+print('Колличество цифр:',count)
+print('Разность суммы и кол-ва цифр:',diff)

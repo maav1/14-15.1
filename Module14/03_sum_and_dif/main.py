fa4ce94b9884d7c7ac97b6c8b6a703696ae0c0a6
maav1 <maav1@yandex.ru>
2021-03-22 12:59:52 +0300

    rename nameing edit PEP8 error delete TODO

--- Module14/03_sum_and_dif/main.py
+++ Module14/03_sum_and_dif/main.py
@@ -1,26 +1,24 @@
-# TODO здесь писать код
-
-def finding_sum_digits(number):
-    summ_digit = 0
-    for digit in number:
-        summ_digit += int(digit)
-    return summ_digit #Возвращает сумму
 
 
-def quantity_digit(number,summ_digit):
-    count = 0
-    while number != 0:
-        count += 1
-        number = number // 10
-    diff = summ_digit - count
-    return count,diff #Возвращает количество и разность
+def finding_sum_digits(number_summ):
+    summ_digit_func = 0
+    for digit in number_summ:
+        summ_digit_func += int(digit)
+    return summ_digit_func  # Возвращает сумму
+
+
+def quantity_digit(number_one, summ_digit_func):
+    count_def = 0
+    while number_one != 0:
+        count_def += 1
+        number_one = number_one // 10
+    diff_func = summ_digit_func - count_def
+    return count_def, diff_func  # Возвращает количество и разность
+
 
 number = input('Введите число: ')
-print('Сумма цифр:',finding_sum_digits(number))
+print('Сумма цифр:', finding_sum_digits(number))
 summ_digit = finding_sum_digits(number)
-count,diff = quantity_digit(int(number),summ_digit)
-print('Колличество цифр:',count)
-print('Разность суммы и кол-ва цифр:',diff)
-
-# TODO удаляем
-# TODO применяем рекомендации данные в 01
+count, diff = quantity_digit(int(number), summ_digit)
+print('Количество цифр:', count)
+print('Разность суммы и кол-ва цифр:', diff)

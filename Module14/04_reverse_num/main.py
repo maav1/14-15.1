7bdff64ca1c03998b862f4e20c77daedf594016a
maav1 <maav1@yandex.ru>
2021-03-22 13:26:27 +0300

    rename nameing edit PEP8 error delete TODO

--- Module14/04_reverse_num/main.py
+++ Module14/04_reverse_num/main.py
@@ -1,20 +1,19 @@
 
-# TODO параметр point должне использоваться
-def enter_data(point):
-    number1 = input('Введите первое число: ')
-    number2 = input('Введите второе число: ')
-    return number1 + '. ' + number2 + '. '
+def enter_data():
+    enter_number1 = input('Введите первое число: ')
+    enter_number2 = input('Введите второе число: ')
+    return enter_number1 + '. ' + enter_number2 + '. '
 
 
-def number_revers(number):
-    num_revers = number[::-1]
+def number_revers(number_for_revers):
+    num_revers = number_for_revers[::-1]
     return num_revers
 
 
 point = ['.']
-number1 = enter_data(point)
+number1 = enter_data()
 probel = [' ']
-total_num =number = []
+total_num = number = []
 second_point = 0
 n = ''
 number_one_revers = []
@@ -33,19 +32,17 @@ for symb in number1:
     else:
         total_num = ("".join(number_one_revers))
 flag = 0
-num1 = num2 = ''
+new_num1 = new_num2 = ''
 for i in total_num:
     if i == ' ':
         flag = 1
 
     if not flag:
-        num1 += i
+        new_num1 += i
     else:
-        num2 += i
-b1 = float(num1)
-c1 = float(num2)
-summ = round(b1 + c1,3)
-print('\nПервое число наоборот:',b1,'\nВторое число наоборот:',c1,'\n\nИх сумма:',summ)
-
-# TODO применяем рекомендации данные в 01
-# TODO нейминг переменных пишем все переменные развернуто
+        new_num2 += i
+first_number_output = float(new_num1)
+second_number_output = float(new_num2)
+summ_output = round(first_number_output + second_number_output, 3)
+print('\nПервое число наоборот:', first_number_output, '\nВторое число наоборот:', second_number_output,
+      '\n\nИх сумма:', summ_output)

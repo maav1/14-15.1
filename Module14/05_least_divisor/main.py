9b0800047ea4459620b6d4220d898fffebeb5269
maav1 <maav1@yandex.ru>
2021-03-22 13:43:37 +0300

    rename nameing edit PEP8 error delete TODO edit return nok(number_nok)

--- Module14/05_least_divisor/main.py
+++ Module14/05_least_divisor/main.py
@@ -1,14 +1,10 @@
-def nok(number):
-    for i in range(2,number+1):
-        if number % i > 0:
+def nok(number_nok):
+    for num in range(2, number_nok+1):
+        if number_nok % num > 0:
             continue
         else:
-            break
-    # TODO i сейчас вне цикла и не может вернуться
-    return i
+            return num
+
 
 number = int(input('Введите число для которого надо найди НОК: '))
-print('Наименьший делитель, отличный от единицы:',nok(number))
-
-# TODO применяем рекомендации данные в 01
-# TODO нейминг переменных пишем все переменные развернуто
+print('Наименьший делитель, отличный от единицы:', nok(number))

3677a7c6fffc7dbfcd4398e03d0aaa1be32e8121
maav1 <maav1@yandex.ru>
2021-03-23 15:20:01 +0300

    edit nameing

--- Module14/03_sum_and_dif/main.py
+++ Module14/03_sum_and_dif/main.py
@@ -1,24 +1,24 @@
 
 
-def finding_sum_digits(number_summ):
-    summ_digit_func = 0
-    for digit in number_summ:
-        summ_digit_func += int(digit)
-    return summ_digit_func  # Возвращает сумму
+def finding_sum_digits(number_summa):
+    summa_digit_inner = 0
+    for digit in number_summa:
+        summa_digit_inner += int(digit)
+    return summa_digit_inner  # Возвращает сумму
 
 
-def quantity_digit(number_one, summ_digit_func):
-    count_def = 0
+def quantity_digit(number_one, summa_digit_inner):
+    counter = 0
     while number_one != 0:
-        count_def += 1
+        counter += 1
         number_one = number_one // 10
-    diff_func = summ_digit_func - count_def
-    return count_def, diff_func  # Возвращает количество и разность
+    difference_func = summa_digit_inner - counter
+    return counter, difference_func  # Возвращает количество и разность
 
 
 number = input('Введите число: ')
 print('Сумма цифр:', finding_sum_digits(number))
-summ_digit = finding_sum_digits(number)
-count, diff = quantity_digit(int(number), summ_digit)
-print('Количество цифр:', count)
-print('Разность суммы и кол-ва цифр:', diff)
+summa_digit = finding_sum_digits(number)
+count_number, difference = quantity_digit(int(number), summa_digit)
+print('Количество цифр:', count_number)
+print('Разность суммы и кол-ва цифр:', difference)

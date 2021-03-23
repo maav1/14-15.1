66ab096ea3e10756b78a170cb243f3e3b48f1e12
maav1 <maav1@yandex.ru>
2021-03-23 15:08:07 +0300

    edit nameing

--- Module14/02_session/main.py
+++ Module14/02_session/main.py
@@ -1,31 +1,27 @@
 
-# TODO функция должна принимать параметры которые не будут пересекаться с глобальными по имени
 def enter_number():
     print("Введите первую точку")
-    entr_x1 = float(input('X: '))
-    entr_y1 = float(input('Y: '))
+    enter_x1 = float(input('X: '))
+    enter_y1 = float(input('Y: '))
     print("\nВведите вторую точку")
-    entr_x2 = float(input('X: '))
-    entr_y2 = float(input('Y: '))
-    return entr_x1, entr_x2, entr_y1, entr_y2
+    enter_x2 = float(input('X: '))
+    enter_y2 = float(input('Y: '))
+    return enter_x1, enter_x2, enter_y1, enter_y2
 
 
-x1, x2, y1, y2 = enter_number()
-x_diff = x1 - x2
-y_diff = y1 - y2
+coordinate_x1, coordinate_x2, coordinate_y1, coordinate_y2 = enter_number()
+x_diff = coordinate_x1 - coordinate_x2
+y_diff = coordinate_y1 - coordinate_y2
 if x_diff != 0 and y_diff != 0:
     k = y_diff / x_diff
-    b = y2 - k * x2
+    b = coordinate_y2 - k * coordinate_x2
     print("Уравнение прямой, проходящей через эти точки:")
     print("y = ", k, " * x + ", b)
 elif x_diff == 0 and y_diff != 0:
-    print('При данных значениях,  функция принимает вид: х = b, где b =', x1,
-          '\nа график функции параллелен оси ОY и смещён по оси ОХ на', x1)
+    print('При данных значениях,  функция принимает вид: х = b, где b =', coordinate_x1,
+          '\nа график функции параллелен оси ОY и смещён по оси ОХ на', coordinate_x1)
 elif x_diff != 0 and y_diff == 0:
-    print('При данных значениях, функция принимает вид: у = b, где b =', y1,
-          '\nа график функции параллелен оси ОX и смещён по оси OY на', y1)
+    print('При данных значениях, функция принимает вид: у = b, где b =', coordinate_y1,
+          '\nа график функции параллелен оси ОX и смещён по оси OY на', coordinate_y1)
 
-# Начиная с этого модуля буду обращать внимание на то что подчеркивает Пайчар. Придерживаемся PEP8
-# Можно привести все к нужному формату code\Reformat code
 
-# TODO Есть недочеты в форматировании по PEP8, используйте пункт меню в пайчарме

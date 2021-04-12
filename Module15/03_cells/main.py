216e1aee6412f80e72aff50e05af7b1b0cf7a28c
maav1 <maav1@yandex.ru>
2021-04-12 16:21:33 +0300

    готово

--- Module15/03_cells/main.py
+++ Module15/03_cells/main.py
@@ -1,1 +1,7 @@
-# TODO здесь писать код
+number_cell = int(input('Введите количество клеток: '))
+cell = []
+for count in range(number_cell):
+  efficiency_cell = int(input(f"Эффективность {count + 1} клетки:"))
+  if efficiency_cell < count + 1:
+     cell.append(str(efficiency_cell))
+print('Не подходящие значения:', *cell)

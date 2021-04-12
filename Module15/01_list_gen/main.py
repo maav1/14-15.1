57029d45f87eed20332dc5581ebe8050c3644670
maav1 <maav1@yandex.ru>
2021-04-12 16:03:56 +0300

    готово

--- Module15/01_list_gen/main.py
+++ Module15/01_list_gen/main.py
@@ -1,1 +1,3 @@
-# TODO здесь писать код
+
+print([i for i in range(1, int(input('Введите число: ')) + 1) if i % 2 != 0])
+#print(range(1, (int(input('Введите число: '))) + 1, 2))  # А так не получается

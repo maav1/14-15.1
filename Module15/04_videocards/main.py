6ab3c024e6c734e9a7bb580c66b6ea5cd30a6972
maav1 <maav1@yandex.ru>
2021-04-12 16:24:22 +0300

    готово

--- Module15/04_videocards/main.py
+++ Module15/04_videocards/main.py
@@ -1,3 +1,12 @@
-# TODO здесь писать код
-
-
+cards = []
+number_videocards = int(input('Количество видеокарт: '))
+print('Введите коды видеокарт:')
+for card in range(number_videocards):
+    cards.append(int(input(f'Видеокарта {card + 1}:')))
+print('Старый список видеокарт:')
+print(cards)
+for element in cards:
+    if int(element) == max(cards):
+        cards.remove(element)
+print('Новый список видеокарт:')
+print(cards)

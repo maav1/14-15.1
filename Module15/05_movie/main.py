93f0fe77d26e7a5a726a7de6b2a0e7e224872167
maav1 <maav1@yandex.ru>
2021-04-12 16:25:33 +0300

    готово

--- Module15/05_movie/main.py
+++ Module15/05_movie/main.py
@@ -1,5 +1,22 @@
+from pprint import pprint
+
 films = ['Крепкий орешек', 'Назад в будущее', 'Таксист',
          'Леон', 'Богемская рапсодия', 'Город грехов',
          'Мементо', 'Отступники', 'Деревня']
+print('Выберите из списка понравившиеся ')
+pprint(films)
 
-# TODO здесь писать код
+films_like = []
+while True:
+    like_film = input('Введите название фильма (введите "стоп" для выхода): ')
+
+    if like_film in films:
+        films_like.append(like_film)
+        print('Фильм', like_film, 'добавлен в избранное.\n')
+    elif like_film == "стоп":
+        break
+    else:
+        print('В списке нет такого фильма.')
+        continue
+print("Список понравившихся фильмов:")
+pprint(films_like)

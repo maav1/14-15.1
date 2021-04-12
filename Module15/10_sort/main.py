79909dc1805fd61eef4f6f189a243805b22ec713
maav1 <maav1@yandex.ru>
2021-04-12 20:41:20 +0300

    готово

--- Module15/10_sort/main.py
+++ Module15/10_sort/main.py
@@ -1,1 +1,8 @@
-# TODO здесь писать код
+
+row_elements = []
+number_elements = int(input('Введите количество элементов списка: '))
+for i in range(number_elements):
+    row_elements.append(int(input(f'Введите элемент {i + 1}: ')))
+print('Список элементов:', row_elements, '\n')
+row_elements.sort()
+print('Отсортированный список:', row_elements)

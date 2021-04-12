fb403783824e704ba5d00bff8ac0224a15c5f523
maav1 <maav1@yandex.ru>
2021-04-12 19:37:02 +0300

    готово

--- Module15/06_word_analysis/main.py
+++ Module15/06_word_analysis/main.py
@@ -1,1 +1,8 @@
-# TODO здесь писать код
+count = 0
+new_word = []
+word = list(input('Введите слово: '))
+for letter in word:
+    if word.count(letter) > 1 and letter not in new_word:
+        count += word.count(letter)
+    new_word.append(letter)
+print('Уникальных букв:', len(word) - count)

8f16e7cbcc89640e507af47a532b857ccf2c8f09
maav1 <maav1@yandex.ru>
2021-04-12 20:19:01 +0300

    готово

--- Module15/09_word_analysis_2/main.py
+++ Module15/09_word_analysis_2/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+
+flag = 0
+word = input('Введите слово: ')
+for i in range(0,(len(word) // 2)):
+    if word[i] != word[-i - 1]:
+        flag = 1
+        break
+if flag:
+    print('Слово не является палиндромом.')
+else:
+    print('Слово является палиндромом.')
